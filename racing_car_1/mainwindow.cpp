11bbda9bd0a9589834d03966d34c9ba362eefcd9
yen-cheng <e125612396@gmail.com>
2024-05-09 20:21:00 +0800

    Done, time counts down

--- racing_car_1/mainwindow.cpp
+++ racing_car_1/mainwindow.cpp
@@ -1,14 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTimer>
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+/*
+0 => init 遊戲被開啟
+1 => playing 
+2 => pause 
+3 => timeout 
+4 => die 
+*/
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    game_status = 0;
+
+    //移動背景
+    bg_pos = 0;
+    object_timer = new QTimer(this);                                         //建立計時器
+    connect(object_timer, SIGNAL(timeout()), this, SLOT(update_objects()));  //計時器與update_objects連結
+    object_timer->start(10);
+
+    // 更新時間
+    time = 30;
+    clock_timer = new QTimer(this);                                         // 建立計時器
+    connect(clock_timer, SIGNAL(timeout()), this, SLOT(update_time()));     // 計時器與update_time連結
+    clock_timer->start(1000);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::update_objects()
+{
+    bg_pos -= 1;
+    ui->background->setGeometry(QRect(bg_pos, 0, 4800, 512));   //背景位置
+
+    if(bg_pos == -32){
+        bg_pos = 0;
+    }
+}
+
+void MainWindow::update_time()
+{
+    time -= 1;
+    ui->lcd_clock->display(time);
+
+    if(time == 0){
+        game_status = 3;
+        game_stop();
+    }
+}
+
+void MainWindow::game_stop()
+{
+    object_timer->stop();
+    clock_timer->stop();
 }
--- racing_car_1/mainwindow.h
+++ racing_car_1/mainwindow.h
@@ -4,20 +4,39 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
+
 namespace Ui {
+
 class MainWindow;
 }
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    private slots:
+        void update_objects();  //移動物件
+        void update_time();     
+
+    private:
+        Ui::MainWindow *ui;
+
+        int game_status;
+        int bg_pos;             //背景水平位置
+        int time;
+
+        QTimer *object_timer;   //移動計時器
+        QTimer *clock_timer;
+
+        void game_start();
+        void game_pause();
+        void game_stop();     
 };
+
 #endif // MAINWINDOW_H
