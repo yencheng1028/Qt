8ebd2743a5e24b226a3216b954120850633934bc
yen-cheng <e125612396@gmail.com>
2024-05-13 11:36:01 +0800

    car move

--- racing_car_1/mainwindow.cpp
+++ racing_car_1/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QTimer>
 #include <QDebug>
+#include <QKeyEvent>
 
 /*
 0 => init 遊戲被開啟
@@ -27,7 +28,7 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     time = 30;
     clock_timer = new QTimer(this);                                         // 建立計時器
     connect(clock_timer, SIGNAL(timeout()), this, SLOT(update_time()));     // 計時器與update_time連結
-    clock_timer->start(1000);
+    //clock_timer->start(1000);
 }
 
 MainWindow::~MainWindow()
@@ -42,6 +43,12 @@ void MainWindow::update_objects()
 
     if(bg_pos == -32){
         bg_pos = 0;
+    }
+
+    if(game_status==1){
+        car_distance += 1;
+        ui->label_distance->setText("行駛距離: " + QString::number(car_distance, 'f',0)+ "公尺");
+        move_car();
     }
 }
 
@@ -60,4 +67,53 @@ void MainWindow::game_stop()
 {
     object_timer->stop();
     clock_timer->stop();
+}
+
+void MainWindow::on_button_start_game_clicked()
+{
+  game_start();
+}
+
+void MainWindow::game_start()
+{
+    game_status = 1;
+    ui->label_title->setVisible(false);
+    ui->button_start_game->setVisible(false);
+    clock_timer->start(1000);
+    time = 30;
+    ui->lcd_clock->display(time);
+
+    car_pos = 220;
+    car_distance = 0;
+    car_direction = 0;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event)
+{
+    switch (event->key()) {
+    case Qt::Key_Up:
+        car_direction = -1;
+        qDebug()<<"up";
+        break;
+
+    case Qt::Key_Down:
+        car_direction = 1;
+        qDebug()<<"down";
+        break;
+    }
+}
+
+void MainWindow::move_car()
+{
+    int car_new_pos = car_pos + car_direction;
+
+    if((car_new_pos) >= 30 && (car_new_pos) <= 410){
+
+        car_pos = car_new_pos;
+        ui->car->setGeometry(QRect(10, car_pos, 237, 71));
+
+    } else {
+        game_status = 4;
+        game_stop();
+    }
 }
--- racing_car_1/mainwindow.h
+++ racing_car_1/mainwindow.h
@@ -20,9 +20,14 @@ class MainWindow : public QMainWindow
         MainWindow(QWidget *parent = nullptr);
         ~MainWindow();
 
+    protected:
+        void keyPressEvent(QKeyEvent* );
+
     private slots:
         void update_objects();  //移動物件
-        void update_time();     
+        void update_time();
+
+        void on_button_start_game_clicked();
 
     private:
         Ui::MainWindow *ui;
@@ -30,13 +35,17 @@ class MainWindow : public QMainWindow
         int game_status;
         int bg_pos;             //背景水平位置
         int time;
+        int car_pos;
+        int car_direction;
+        float car_distance;
 
         QTimer *object_timer;   //移動計時器
         QTimer *clock_timer;
 
         void game_start();
         void game_pause();
-        void game_stop();     
+        void game_stop();
+        void move_car();
 };
 
 #endif // MAINWINDOW_H
